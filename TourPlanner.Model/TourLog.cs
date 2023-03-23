0cb5942898c31dbd20dabf4be028c127d1d565ce
Simon Penn <smnpenn@gmail.com>
2023-03-23 15:38:03 +0100

    AddTour/AddTourLogs now create actual objects in DAL

--- TourPlanner.Model/TourLog.cs
+++ TourPlanner.Model/TourLog.cs
@@ -12,8 +12,25 @@ namespace TourPlanner.Model
         public Tour RelatedTour { get; set; }
         public DateTime DateTime { get; set; } = DateTime.Now;
         public String Comment { get; set; } = String.Empty;
-        public String Difficulty { get; set; } = "Easy"; //make enum?
-        public int TotalTime { get; set; } //time in min
-        public double Rating { get; set; } //0-5 stars
+        public double Difficulty { get; set; } = 0; //make enum?
+        public int TotalTime { get; set; } = 0; //time in min
+        public double Rating { get; set; } = 0; //0-5 stars
+
+        public TourLog(string name, Tour relatedTour, DateTime dateTime, string comment, double difficulty, int totalTime, double rating)
+        {
+            Name = name;
+            RelatedTour = relatedTour;
+            DateTime = dateTime;
+            Comment = comment;
+            Difficulty = difficulty;
+            TotalTime = totalTime;
+            Rating = rating;
+        }
+
+        public TourLog(string name, Tour relatedTour)
+        {
+            Name = name;
+            RelatedTour = relatedTour;
+        }
     }
 }
