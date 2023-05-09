057e9e10a6f5d3de125c95dac66d401dc42d73b1
Simon Penn <smnpenn@gmail.com>
2023-05-09 09:57:23 +0200

    fixed some things

--- TourPlanner.Model/TourLog.cs
+++ TourPlanner.Model/TourLog.cs
@@ -15,7 +15,7 @@ namespace TourPlanner.Model
         [Newtonsoft.Json.JsonIgnore]
         public Tour RelatedTour { get; set; }
         public DateTime DateTime { get; set; } = DateTime.Now;
-        public String? Comment { get; set; } = String.Empty;
+        public String? Comment { get; set; }
         public double Difficulty { get; set; } = 0; //make enum?
         public int TotalTime { get; set; } = 0; //time in min
         public double Rating { get; set; } = 0; //0-5 stars
