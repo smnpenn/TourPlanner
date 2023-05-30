34e4694610809da701699acc1daa46ea0bd6fcdd
Maximilian Turza <maxturza@hotmail.com>
2023-05-30 08:09:05 +0200

    reworked ElasticSearch-Entries and added v8.x.x support

--- TourPlanner.DAL/ElasticSearch/ElasticSearchService.cs
+++ TourPlanner.DAL/ElasticSearch/ElasticSearchService.cs
@@ -1,4 +1,7 @@
-﻿using Nest;
+﻿using Elasticsearch.Net;
+using Nest;
+using System.Collections.ObjectModel;
+using TourPlanner.Model;
 
 namespace TourPlanner.DAL.ElasticSearch
 {
@@ -7,34 +10,105 @@ namespace TourPlanner.DAL.ElasticSearch
         private readonly ElasticClient _client;
         public ElasticSearchService()
         {
-            var settings = new ConnectionSettings(new Uri("http://localhost:9200"))
-            .DefaultIndex("my-index-001");
-
+            var pool = new SingleNodeConnectionPool(new Uri("https://localhost:9200"));
+            var settings = new ConnectionSettings(pool)
+                .CertificateFingerprint("54:44:ec:01:47:37:9d:9f:58:dd:1a:be:54:bf:3b:4c:72:0e:a6:c7:19:67:60:ab:63:19:fc:0f:00:ee:53:ce")
+                .BasicAuthentication("elastic", "elastic")
+                .DefaultIndex("testindex-001")
+                .DefaultMappingFor<ElasticTourDocument>(i => i.IndexName("tours-v1"))
+                // .DefaultMappingFor<ElasticTourLogDocument>(i => i.IndexName("tourlogs-v1"))
+                .EnableApiVersioningHeader();
             _client = new ElasticClient(settings);
 
         }
 
 
-        public ISearchResponse<T> Search<T>(SearchDescriptor<T> searchDescriptor) where T : class
+        public async Task<string> IndexTourDocument(int doc_id, string name, string description, string from, string to, int index, ObservableCollection<TourLog> logs)
         {
-            return _client.Search<T>(searchDescriptor);
+
+            var data = new ElasticTourDocument(doc_id, name, description, from, to, logs);
+
+            var response = await _client.IndexAsync(
+                data, idx => idx
+                .Id(index)
+            );
+
+            if (response.Result == Result.Created)
+            {
+                Console.WriteLine("Successfully created!");
+                return response.ToString();
+            }
+            else
+            {
+                Console.WriteLine("Failed to create document!");
+                return "failed";
+            }
         }
 
-        public ISearchResponse<T> Search<T>(Func<SearchDescriptor<T>, ISearchRequest> selector) where T : class
+
+        // Search 
+        // TO-DO: Search in Logs?
+
+        public void FuzzySearchLogs(string givenString)
         {
-            return _client.Search(selector);
+
+            var response = _client.Search<ElasticTourDocument>(s => s
+            .Query(q => q
+            .MultiMatch(c => c
+            .Fields(f => f
+                .Field(p => p.Name)
+                .Field(p => p.From)
+                .Field(p => p.To)
+                .Field(p => p.Description)
+             )
+            .Query(givenString)
+            .Fuzziness(Fuzziness.Auto)
+            )));
+
+            var docs = response.Documents;
+
+            Console.WriteLine($"response:  {docs.Count}");
+
+            foreach (var doc in docs)
+            {
+                Console.WriteLine($" Name: {doc.Name} Bio: {doc.Description} ");
+            }
         }
 
-        // INDEXING
-        public IndexResponse Index<T>(IIndexRequest<T> indexRequest) where T : class
+        // Update documents
+        public async Task<string> UpdateTourDocument(string id, int doc_id, string name, string description, string from, string to, ObservableCollection<TourLog> logs)
         {
-            return _client.Index(indexRequest);
+            var updateRequest = new UpdateRequest<ElasticTourDocument, ElasticTourDocument>(id)
+            {
+                Doc = new ElasticTourDocument(doc_id, name, description, from, to, logs)
+
+            };
+
+            var updateResponse = await _client.UpdateAsync(updateRequest);
+
+            if (updateResponse.IsValid)
+            {
+                return "Document successfully updated";
+            }
+            else
+            {
+                return "There was an error updating the document";
+            }
         }
 
-        // DELETE
-        public DeleteResponse Delete<T>(IDeleteRequest deleteRequest) where T : class
+        // Delete Documents
+        public string DeleteTourDocument(string id)
         {
-            return _client.Delete(deleteRequest);
+            var deleteResponse = _client.Delete<ElasticTourDocument>(id);
+
+            if (deleteResponse.IsValid)
+            {
+                return "Document deleted successfully";
+            }
+            else
+            {
+                return "Failed to delete the document";
+            }
         }
 
 
---
+++ TourPlanner.Model/ElasticTourDocument.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+
+namespace TourPlanner.Model
+{
+    public class ElasticTourDocument
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String? Description { get; set; }
+        public String From { get; set; }
+        public String To { get; set; }
+
+        public ObservableCollection<TourLog> Logs { get; set; }
+
+        public ElasticTourDocument(int id, string name, string description, string from, string to, ObservableCollection<TourLog> logs)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            From = from;
+            To = to;
+            Logs = logs;
+        }
+    }
+}
